caaa5cf026ba9c6df758427d1bc8e250410f4271
xxFlo04 <florian.dunstheimer@gmail.com>
2026-06-03 14:57:29 +0200

    moved footer part of old to new footer tsx and added function in app

---
+++ src/components/footer.tsx
@@ -0,0 +1,17 @@
+export function Footer(){
+	return (
+		<div id="ende">
+			<ul>
+				<li>
+				Ersteller:
+				<ul>
+					<li>L.Mayer</li>
+					<li>N.Plötz</li>
+					<li>F.Dunstheimer</li>
+				</ul>
+				</li>
+				<li><a href="https://github.com/LeReyam/Satiscalc">Unser Repository</a></li>
+			</ul>
+	</div>
+	)
+}
