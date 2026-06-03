35fb90fe56f3d3a8ece0c6ea8e5a78903779a50a
xxFlo04 <florian.dunstheimer@gmail.com>
2026-06-03 15:32:21 +0200

    add About, Costum_recipe_editor, and Costum_recipetable components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+import { About } from "./components/about.tsx";
+import { Costum_recipe_editor } from "./components/costum-recipe-editor.tsx";
+import { Costum_recipetable } from "./components/costum-recipetable.tsx";
 import { Footer } from "./components/footer";
 import "./App.css";
 import {Header} from "./components/header.tsx"
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
 		<Header/>
-      
+    <About/>
+    <Costum_recipe_editor/>
+    <Costum_recipetable/>
 		<Footer />
     </>
   );
---
+++ src/components/about.tsx
@@ -0,0 +1,18 @@
+export function About(){
+	return(
+		<div title="about">
+			<section>
+				<h2>Ersteller</h2>
+				<ul>
+				<li>L.Mayer</li>
+				<li>N.Plötz</li>
+				<li>F.Dunstheimer</li>
+				</ul>
+				<h2>Projekt</h2>
+				<p>
+				<a href="https://github.com/LeReyam/Satiscalc">Unser Repository</a>
+				</p>
+			</section>
+		</div>
+	)
+}
