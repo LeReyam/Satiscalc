5bddd552bfb4ad23adf278cf1076b1a18027a25e
LeReyam <Leon.Mayer@htwg-konstanz.de>
2026-06-03 14:57:49 +0200

    added header to app

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+export function Header() {
+  return (
+    <header id="start">
+		<h1>Productionplanner</h1>
+		<nav>
+			<a href="index.html#start">Start</a>
+			<a href="index.html#planner">Planner</a>
+			<a href="custom-recipies.html">Custom-Rezept</a>
+			<a href="about.html">About</a>
+			<a href="login.html">Login</a>
+		</nav>
+	</header>
+  );
+}
