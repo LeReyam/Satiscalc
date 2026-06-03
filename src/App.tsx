0f389fe27e907a0f3d17eb01bddf563aa1aff596
xxFlo04 <florian.dunstheimer@gmail.com>
2026-06-03 15:57:12 +0200

    feat: add Productionplanner_output component to App for enhanced resource display

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { About } from "./components/about.tsx";
 import { Costum_recipe_editor } from "./components/costum-recipe-editor.tsx";
 import { Costum_recipetable } from "./components/costum-recipetable.tsx";
+import { Productionplanner_output } from "./components/productionplanner-output.tsx";
 import { Footer } from "./components/footer";
 import { Recipe_selector } from "./components/recipeselector"
 import "./App.css";
@@ -17,6 +18,10 @@ function App() {
 		<Costum_recipe_editor/>
 		<Costum_recipetable/>
 		<Recipe_selector/>
+    <About/>
+    <Costum_recipe_editor/>
+    <Costum_recipetable/>
+    <Productionplanner_output/>
 		<Footer />
     </>
   );
