d496b5b4156e23c05ad0f867f5db4b316682279c
Magnum-magnus <nicholas-2@web.de>
2026-08-01 15:09:03 +0200

    worked on some tests

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
-
+['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
 export default defineConfig({
   plugins: [react()],
   test: {
